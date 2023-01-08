4b808b897f48cdf97a7399dead9001661e7216eb
Tom C <mysophobicgamer@gmail.com>
2023-01-07 20:08:08 -0500

    added bcrypt to encrypt user password before sending to database

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const user = express.Router();
 
+const bcrypt = require('bcrypt')
+
 const User = require('../models/user');
 
 // CREATE
 user.post('/signup', async (req, res) => {
     try {
+      let { password, ...etc } = req.body
+
        const user = await new User({
-          ...req.body
+          ...etc, 
+          passwordDigest: await bcrypt.hash( password, 16)
        }).save()
  
        res.send(user)
+       res.redirect('/')
  
     } catch (error) {
        res.status(500).json({message: `Unable to add User: ${ error }`})
